aaed9be5e515c7ffed91b405c027adc31988b153
Bakakemono <robinalves1224@gmail.com>
2019-12-16 15:08:20 +0100

    Merge of "master" and "origin/master"

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Settings")]
+    [SerializeField] float speed = 1;
+
+    [Header("Jump Settings")]
+    [SerializeField] AnimationCurve Curve;
+    [SerializeField] float height = 1;
+    [SerializeField] float duration = 1;
+
+    
+}
