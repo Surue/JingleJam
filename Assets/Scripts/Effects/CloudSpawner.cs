efd86a3c019f19658e8c426d8a90deda63e4fbf7
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-12-17 14:07:54 +0100

    fix non updating spwing position of clouds

--- Assets/Scripts/Effects/CloudSpawner.cs
+++ Assets/Scripts/Effects/CloudSpawner.cs
@@ -15,14 +15,18 @@ public class CloudSpawner : MonoBehaviour, LevelManager.IPausedListener {
 
     float timer = 0;
 
-    [SerializeField] float spawnPointX = 10;
+    [SerializeField] float offsetSpawnPointX = 10;
 
     bool isPaused = false;
+
+    PlayerController player;
     
     // Start is called before the first frame update
     void Start()
     {
         LevelManager.Instance.AddPauseListener(this);
+
+        player = LevelManager.PlayerController;
     }
 
     // Update is called once per frame
@@ -32,17 +36,29 @@ public class CloudSpawner : MonoBehaviour, LevelManager.IPausedListener {
         timer -= Time.deltaTime;
 
         if (timer < 0) {
+            float offsetX = player.transform.position.x;
             timer = Random.Range(minTime, maxTime);
 
             GameObject cloud = Instantiate(prefabCloud);
-            cloud.transform.position = new Vector3(spawnPointX, Random.Range(minHeight, maxHeight), 0);
+            cloud.transform.position = new Vector3(offsetSpawnPointX + offsetX, Random.Range(minHeight, maxHeight), 0);
         }
     }
 
     void OnDrawGizmos() {
-        Gizmos.DrawLine(new Vector3(spawnPointX, maxHeight, 0), new Vector3(spawnPointX, minHeight, 0));
-        Gizmos.DrawLine(new Vector3(spawnPointX - 1, maxHeight, 0), new Vector3(spawnPointX + 1, maxHeight, 0));
-        Gizmos.DrawLine(new Vector3(spawnPointX - 1, minHeight, 0), new Vector3(spawnPointX + 1, minHeight, 0));
+        if (player) {
+            float offsetX = player.transform.position.x;
+            Gizmos.DrawLine(new Vector3(offsetSpawnPointX + offsetX, maxHeight, 0), new Vector3(offsetSpawnPointX  + offsetX, minHeight, 0));
+            Gizmos.DrawLine(new Vector3(offsetSpawnPointX - 1  + offsetX, maxHeight, 0),
+                new Vector3(offsetSpawnPointX + 1  + offsetX, maxHeight, 0));
+            Gizmos.DrawLine(new Vector3(offsetSpawnPointX - 1  + offsetX, minHeight, 0),
+                new Vector3(offsetSpawnPointX + 1  + offsetX,  minHeight, 0));
+        } else {
+            Gizmos.DrawLine(new Vector3(offsetSpawnPointX, maxHeight, 0), new Vector3(offsetSpawnPointX, minHeight, 0));
+            Gizmos.DrawLine(new Vector3(offsetSpawnPointX - 1, maxHeight, 0),
+                new Vector3(offsetSpawnPointX + 1, maxHeight, 0));
+            Gizmos.DrawLine(new Vector3(offsetSpawnPointX - 1, minHeight, 0),
+                new Vector3(offsetSpawnPointX + 1, minHeight, 0));
+        }
     }
 
     public void OnPaused() {
