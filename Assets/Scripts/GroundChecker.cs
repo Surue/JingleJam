e3aa98517c832a9641cabc4761d7feb8135100d4
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-12-17 09:15:12 +0100

    Play impact particle with player

--- Assets/Scripts/GroundChecker.cs
+++ Assets/Scripts/GroundChecker.cs
@@ -6,7 +6,27 @@ using UnityEngine;
 public class GroundChecker : MonoBehaviour {
     bool isGrounded = false;
 
+    [SerializeField] ParticleSystem particleSystem;
+    Rigidbody2D body;
+    
     public bool IsGrounded => isGrounded;
+
+    bool isFalling = false;
+
+    void Start() {
+        body = transform.parent.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        isFalling = body.velocity.y < -1;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.layer != LayerMask.NameToLayer("Ground")) return;
+        if (isFalling) {
+            particleSystem.Play();
+        }
+    }
 
     void OnTriggerStay2D(Collider2D other) {
         if (other.gameObject.layer == LayerMask.NameToLayer("Ground")) {
